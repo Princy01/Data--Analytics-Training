38fa0dcf6b7875a8c540de8614f45e418087ff95
princyraina <princyraina70@gmail.com>
2024-11-22 17:28:56 +0530

    final commit

--- methods.py
+++ methods.py
@@ -1,11 +1,19 @@
 #function inside class is method,outside class is function.In python everything is inside class,so there is only method
 
-def sum():      #method declared and defined
-    a=int(input("Enter number 1: "))
-    b=int(input("Enter number 2: "))
+def sum():   # method declare + define 
+    a=int(input("Enter number 1 "))
+    b=int(input("Enter number 2 "))
 
     c=a+b
     return c
 
-r1=sum()
-print(r1)
+r1 =sum()
+print("r1= ",r1)
+
+r2 =sum()
+print("r2= ",r2)
+
+r3 =sum()
+print("r3= ",r3)
+    
+
